8dbb204f9451e4ad1822ea41d956fae833529403
ShubhamTiwari909 <shubhmtiwri00@gmail.com>
2025-09-17 12:49:16 +0530

    build error fixes

--- apps/frontend/src/components/blogs/CodeRenderer.tsx
+++ apps/frontend/src/components/blogs/CodeRenderer.tsx
@@ -1,24 +1,15 @@
-"use client"
-import Prism from 'prismjs'
-import 'prismjs/themes/prism-tomorrow.css'
-import { useEffect, useState } from 'react'
+import React from 'react'
 
-export default function CodeRenderer({ code, language = "javascript"}: { code: string, language?: string }) {
-    const [isCopied, setIsCopied] = useState(false)
-  useEffect(() => {
-    Prism.highlightAll()
-  }, [code])
+interface CodeRendererProps {
+  code: string
+}
 
+export default function CodeRenderer({ code }: CodeRendererProps) {
   return (
-    <pre suppressHydrationWarning className={`language-${language} relative !my-5`}>
-      <code className={`language-${language}`}>{code}</code>
-      <button className='absolute top-4 right-4 cursor-pointer' onClick={() => {
-        navigator.clipboard.writeText(code)
-        setIsCopied(true)
-        setTimeout(() => {
-          setIsCopied(false)
-        }, 2000)
-      }}> {isCopied ? <span className='text-sm text-green-500'>Copied</span> : 'Copy'}</button>
-    </pre>
+    <div className="mx-auto lg:max-w-300 px-5">
+      <pre className="bg-slate-900 text-slate-100 p-6 rounded-xl overflow-x-auto shadow-lg border border-slate-700 font-mono text-sm leading-relaxed whitespace-pre-wrap">
+        <code>{code}</code>
+      </pre>
+    </div>
   )
 }
