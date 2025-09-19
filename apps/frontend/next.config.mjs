d7cd1ce2edaac69fc76ca61b4b24562befc67c62
ShubhamTiwari909 <shubhmtiwri00@gmail.com>
2025-09-19 14:32:37 +0530

    added headers to resolve cors issue for backend url

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -12,6 +12,20 @@ const nextConfig = {
 
     return webpackConfig
   },
+  async headers() {
+    return [
+      {
+        // Apply to all routes
+        source: "/(.*)", 
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://blazing-blogs-backend.vercel.app" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
