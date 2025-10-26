6592be4706c580b65498c7870b2d74b2c197e6b3
ShubhamTiwari909 <shubhmtiwri00@gmail.com>
2025-10-26 21:01:07 +0530

    removed force static from collaborators page

--- apps/frontend/src/app/(frontend)/collaborators/page.tsx
+++ apps/frontend/src/app/(frontend)/collaborators/page.tsx
@@ -1,8 +1,6 @@
 import { queryCollaborators } from '@/lib/fetch-utils/query-all-pages'
 import Collaborators from '@/components/collaborators/Collaborators'
 
-export const dynamic = 'force-static'
-
 const CollaboratorsPage = async () => {
   const collaborators = await queryCollaborators()
   return <Collaborators collaborators={collaborators} />
