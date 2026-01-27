2b3163a884a54390ca0264bfa97eb69c8a495cb8
ShubhamTiwari909 <shubhmtiwri00@gmail.com>
2026-01-27 19:23:49 +0530

    moved blogs list fetching inside a suspense boundary

--- apps/frontend/src/app/(frontend)/blogs/page.tsx
+++ apps/frontend/src/app/(frontend)/blogs/page.tsx
@@ -1,29 +1,16 @@
-import { getCachedPageData } from '@/lib/fetch-utils/fetch-blogs'
-import BlogsList from '@/components/blogs/blogs-list/BlogsList'
-import { Typography } from '@/components/atoms/typography'
 import Header from '@/components/blogs/blogs-list/Header'
 import { METADATA } from './metadata'
+import { Suspense } from 'react'
+import BlogsListWrapper from '@/components/blogs/blogs-list/BlogsListWrapper'
 
 export const metadata = METADATA
 
 const page = async () => {
-  const pages = await getCachedPageData()
   return (
     <div className="min-h-screen bg-gray-50 py-12">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <Header totalDocs={pages?.totalDocs || 0} />
-        {pages && pages.docs.length > 0 ? (
-          <BlogsList pages={pages} />
-        ) : (
-          <div className="py-12 text-center">
-            <Typography as="p" size="base" color="inherit" className="text-gray-500">
-              No blogs found
-            </Typography>
-            <Typography as="p" size="base" color="inherit" className="mt-2 text-gray-400">
-              Check back later for new content!
-            </Typography>
-          </div>
-        )}
+        <Header />
+        <Suspense><BlogsListWrapper /></Suspense>
       </div>
     </div>
   )
--- apps/frontend/src/components/blogs/blogs-list/Header.tsx
+++ apps/frontend/src/components/blogs/blogs-list/Header.tsx
@@ -1,17 +1,12 @@
 import AnimationBox from '@/components/ui/animations/AnimationBox'
 import { Typography } from '@/components/atoms/typography'
-import { LuDock } from 'react-icons/lu'
 
 import SearchInput from './SearchInput'
 
-const Header = ({ totalDocs }: { totalDocs: number }) => {
+const Header = () => {
   return (
     <div className="mb-16 text-center">
       <SearchInput />
-      <AnimationBox className="mb-6 inline-flex items-center gap-2 rounded-full bg-blue-50 px-4 py-2 text-sm font-medium text-blue-600">
-        <LuDock className="h-4 w-4" />
-        {totalDocs} Blogs
-      </AnimationBox>
       <AnimationBox>
         <Typography
           as="h1"
