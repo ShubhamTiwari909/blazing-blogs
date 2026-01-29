b2fac8773546d5f30efd9ba17de0a73670e4c8b2
ShubhamTiwari909 <shubhmtiwri00@gmail.com>
2026-01-29 16:33:05 +0530

    not found page double header footer issue fixed

--- apps/frontend/src/app/(frontend)/blogs/not-found.tsx
+++ apps/frontend/src/app/(frontend)/blogs/not-found.tsx
@@ -1,12 +1,9 @@
 
-import RootLayout from '@/components/layout/RootLayout'
 import NotFound from '@/components/layout/NotFound'
 
 const NotFoundGlobal = () => {
   return (
-    <RootLayout>
-      <NotFound />
-    </RootLayout>
+    <NotFound />
   )
 }
 
